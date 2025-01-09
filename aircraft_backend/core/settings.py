8f3e46fa6df78e121f9460177b290662c4f21692
alikilicw <alikilic.w1@gmail.com>
2025-01-09 10:08:53 +0300

    updated settings

--- aircraft_backend/core/settings.py
+++ aircraft_backend/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j(r@u-cdg6%seh2pm6r97-*#15%rv9oct382ug++^)rp&^(m20
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
