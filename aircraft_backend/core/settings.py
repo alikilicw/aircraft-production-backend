272148279ede6f0a303a779d2357713998800c5c
alikilicw <alikilic.w1@gmail.com>
2025-01-09 10:45:33 +0300

    added settings

--- aircraft_backend/core/settings.py
+++ aircraft_backend/core/settings.py
@@ -144,7 +144,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://aircraft-nextjs:3000'
-]
+CORS_ALLOW_ALL_ORIGINS = True
